85c187eb354f6e5db3c650b553cce7977fd2d3f3
Parth Acharya <1008parth@gmail.com>
2025-01-26 13:09:28 +0530

    Updates POST /parse-csv to interact with supabase

--- server/routes.py
+++ server/routes.py
@@ -1,13 +1,14 @@
 from server import app, supabaseClient
 
 from flask import Response, jsonify, g
-from werkzeug.exceptions import BadRequest
+from werkzeug.exceptions import BadRequest, InternalServerError
 
 import pandas as pd
 
 from auxillary import validate_CSV, enforce_JSON, require_token
 
 from datetime import datetime
+from uuid import uuid4
 
 @app.route("/parse-csv", methods=["POST"])
 @require_token
@@ -18,7 +19,31 @@ def storeCSV():
     except:
         raise BadRequest("CSV file could not be parsed properly. Ensure proper file format, size, encoding, and properly formatted columns.")
     
-    supabaseClient.storage().from_(g.tkn["sub"]).upload(g.CSV_FILE)
+    epoch = datetime.now()
+    filename = f"{g.CSV_FILE}_{epoch.strftime('%H%M%S%d%m%y')}_{uuid4.hex}.csv"
+
+    response = supabaseClient.storage().from_(g.tkn["sub"]).upload(filename)
+    if response.get("error"):
+        raise InternalServerError(f"Error uploading file: {response['error']}")
+    
+    analysis_response = supabaseClient.from_("analysis").insert({
+        "score": None,
+        "file_name": filename,
+        "user_id": g.tkn["sub"],
+        "category": None
+    }).execute()
+
+    if analysis_response.get("error"):
+        raise InternalServerError(f"Error inserting into 'analysis': {analysis_response['error']}")
+
+    # Update the 'records' column in the 'users' table
+    update_response = supabaseClient.from_("users").update({
+        "records": supabaseClient.raw("records + 1")
+    }).eq("uuid", g.tkn["sub"]).execute()
+
+    if update_response.get("error"):
+        raise InternalServerError(f"Error updating 'users': {update_response['error']}")
+
     return jsonify({"user" : g.tkn["sub"],
-                    "epoch" : datetime.strftime(datetime.now(), "%H:%M:%S, %d/%m/%y"),
-                    "filename" : g.CSV_FILE.name}), 200
+                    "epoch" : datetime.strftime(epoch, "%H:%M:%S, %d/%m/%y"),
+                    "sb_filename" : filename}), 200
